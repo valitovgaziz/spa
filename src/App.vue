c35d13779273bdd24e08767be6f47640ece9f657
valitovgaziz <valitovgaziz@gmail.com>
2024-10-20 18:55:09 +0500

    Second exercise is complite

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
+// src/App.vue
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Exercise />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Exercise from './components/Exercise1-02.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Exercise,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Exercise1-01.vue
@@ -0,0 +1,22 @@
+// src/component/Exercise1-01.vue
+
+<template>
+    <div>
+        <h1>My first component!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    h1 {
+        font-family: 'Avenir', Arial, Helvetica, sans-serif;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
